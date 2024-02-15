e5ea37c0eb7823e4fcb1a9649d76054e404b3014
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2024-02-15 09:24:53 +0200

    added auto increment ids

--- src/database/schemas/customers/customers.sql
+++ src/database/schemas/customers/customers.sql
@@ -1,6 +1,6 @@
 --@block
 CREATE TABLE "Customers" (
-  "CustomerID" bigint PRIMARY KEY,
+  "CustomerID" SERIAL PRIMARY KEY,
   "Name" varchar(255),
   "Email" varchar(255) UNIQUE,
   "PhoneNumber" varchar(255)
